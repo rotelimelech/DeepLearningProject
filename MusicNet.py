a9e1ebc66529365763e6c1ff31ba07efdc2b6b3a
Rotem Elimelech <rotelimelech@gmail.com>
2023-01-10 21:26:06 +0200

    MusicNet dataset complete for single note

--- MusicNet.py
+++ MusicNet.py
@@ -9,7 +9,7 @@ import torchaudio
 USED_COLUMNS = ['start_time', 'end_time', 'instrument', 'note']
 # The NN should receive a vector in a standard length. 
 # This is the median length of a note in the dataset
-STANDARDIZE_SIZE = 10240 
+STANDARDIZE_SIZE = 10240
 
 class MusicNet(Dataset):
 	"""
@@ -19,15 +19,21 @@ class MusicNet(Dataset):
 	each contains a single note in a standardize length
 	"""
 
-	def __init__(self, dataset_path, groups=['train', 'test']):
+	def __init__(self, dataset_path, groups=['train', 'test'], transform=None):
 		self.dataset_path = dataset_path
+		self.transform = transform
 
+		if isinstance(groups, list):
+			self.groups = groups
+		else:
+			self.groups = [groups, ]
+
+		
 		# loading the entire metadata when creating the object.
 		# Since our "true samples" are individual notes found 
 		# across different files, we must load all of the separate
 		# files metadata to index them properly.
 		self.all_metadata = self._load_metadata(groups)
-
 
 	def _load_metadata(self, groups):
 		joined_metadata_cols = ['csv_id', 'group'] + USED_COLUMNS
@@ -61,15 +67,19 @@ class MusicNet(Dataset):
 		is of `STANDARDIZE_SIZE` length
 		"""
 		_, length = waveform.shape
-		if length <= STANDARDIZE_SIZE:
+		if length >= STANDARDIZE_SIZE:
 			return waveform[:,:STANDARDIZE_SIZE]
 		else:
 			copies_needed = 1 + STANDARDIZE_SIZE // length
 			duplicated_wf = torch.cat([waveform for _ in range(copies_needed)], 1)
 			return duplicated_wf[:,:STANDARDIZE_SIZE]
 
-
 	def __getitem__(self, index):
+		"""
+		Returns the `index`-th item as:
+		[waveform, instrument, note]
+		Notice - self.transform acts on waveform
+		"""
 		sample_data = self.all_metadata.iloc[index]
 		sample_wav_path = join(
 			self.dataset_path, 
@@ -77,10 +87,13 @@ class MusicNet(Dataset):
 			f"{sample_data['csv_id']}.wav")
 		waveform, sample_rate = torchaudio.load(sample_wav_path)
 
-		clipped_sample = waveform[:,sample_data['start_time']:sample_data['end_time']]
-		# todo:
-		# Create a dict from all of the data processed here and return it 
-		# in the correct format 
-		return clipped_sample
+		clipped_sample = waveform[:, sample_data['start_time']:sample_data['end_time']]
+		clipped_sample = self._norm_waveform_len(clipped_sample)
+
+		if self.transform:
+			clipped_sample = self.transform(clipped_sample)
+		
+		return clipped_sample, sample_data['instrument'], sample_data['note']
+
 	def __len__(self):
 		return len(self.all_metadata)
