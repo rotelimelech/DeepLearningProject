c6106cb6dc165dfa0417be9b6033ed81a0052d58
Rotem Elimelech <rotelimelech@gmail.com>
2023-01-15 19:03:23 +0200

    trainning on a small portion and adding a single linear layer

--- train.py
+++ train.py
@@ -12,24 +12,44 @@ BATCH_SIZE = 10
 Train the model using MusicNet dataset
 """
 
+def get_test_score(test_loader, model):
+    correct = 0
+    with torch.no_grad():
+        for batch_id, (batch_data, instrument, note) in enumerate(train_loader):
+            output = model(batch_data)
+            prediction = output.argmax(dim=1, keepdim=True)
+            correct += pred.eq(instrument.view_as(pred)).sum().item()
+
+    n_test_samples = len(test_loader.dataset)
+    return correct / n_test_samples
+
 def main():
-    dataset = MusicNet(
+    train_dataset = MusicNet(
         '.\\MusicNet',
-        metadata_path='./MusicNet/all_metadata_processed_150123.csv',
+        # metadata_path='./MusicNet/all_metadata_processed_150123.csv',
+        metadata_path='./MusicNet/all_local_metadata_150123.csv',
         indexes_paths='./MusicNet/inst_and_note_index_150123.json',
         load_group='train',
-        # groups='train', # something broken in loading only one group
         transform=transforms.Spectrogram()
     )
+    train_loader =  torch.utils.data.DataLoader(train_dataset, batch_size=BATCH_SIZE, shuffle=True)
 
-    train_loader =  torch.utils.data.DataLoader(dataset, batch_size=BATCH_SIZE, shuffle=True)
-    
+    test_dataset = MusicNet(
+        '.\\MusicNet',
+        # metadata_path='./MusicNet/all_metadata_processed_150123.csv',
+        metadata_path='./MusicNet/all_local_metadata_150123.csv',
+        indexes_paths='./MusicNet/inst_and_note_index_150123.json',
+        load_group='test',
+        transform=transforms.Spectrogram()
+    )
+    test_loader =  torch.utils.data.DataLoader(test_dataset, batch_size=BATCH_SIZE, shuffle=True)
+
     # The other model is broken :(
     model = models.resnet18(weights='DEFAULT')
     model = nn.Sequential(
         model,
         # to the correct output size
-        nn.Linear(1000, dataset.n_instruments),
+        nn.Linear(1000, train_dataset.n_instruments),
         # manyhot 
         nn.Sigmoid()
         )
@@ -37,17 +57,26 @@ def main():
     loss_fn = torch.nn.MSELoss()
     opt = torch.optim.SGD(model.parameters(), lr=0.001)
 
-    for batch_data, instrument, note in train_loader:
-        # resnet excepts three channels of input
-        batch_multi_channel = batch_data.repeat(1,3,1,1)
-        
-        # train
-        output = model.forward(batch_multi_channel)
-        loss = loss_fn(output, instrument.float())
-        opt.zero_grad()
-        loss.backward()
-        opt.step()
+    num_epochs = 1
 
+    model.train()
+    for epoch_id in range(num_epochs):
+        for batch_id, (batch_data, instrument, note) in enumerate(train_loader):
+            # resnet excepts three channels of input
+            batch_multi_channel = batch_data.repeat(1,3,1,1)
+            
+            # train
+            output = model.forward(batch_multi_channel)
+            loss = loss_fn(output, instrument.float())
+            opt.zero_grad()
+            loss.backward()
+            opt.step()
+
+            print(f'epoch - {epoch_id}, batch - {batch_id}, loss - {loss}')
+
+    # checking performence 
+    test_accuracy = get_test_score(test_loader, model)
+    print(f'test accuracy is {test_accuracy:.2f}%')
     # might need to add 
     # to make resnet use only one channel
 
