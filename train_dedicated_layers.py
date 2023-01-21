68d1438fb4e6c8017d367109958bd000119b0520
Rotem Elimelech <rotelimelech@gmail.com>
2023-01-21 12:14:02 +0200

    using val set

--- train_dedicated_layers.py
+++ train_dedicated_layers.py
@@ -12,7 +12,7 @@ from torch.optim.lr_scheduler import StepLR
 from torchmetrics import JaccardIndex # IoU loss
 
 BATCH_SIZE = 10
-N_EPOCHS = 20
+N_EPOCHS = 10
 SAMPLES_PER_BATCH = 200
 
 # Enums for type of target to detect
@@ -25,7 +25,7 @@ def save_model(model, folder, name):
     torch.save(model, path_join(folder, f'{name}_{now}'))
 
 
-def train_single_layer(base_model, train_loader, target, lr=0.001,
+def train_single_layer(base_model, train_loader, val_loader, target, lr=0.001,
         scheduler_step_size=1, scheduler_gamma=0.5):
     """
     Adds a single linear layer to `base_model` and trains it to detect
@@ -76,32 +76,31 @@ def train_single_layer(base_model, train_loader, target, lr=0.001,
             print(f'epoch - {epoch_id}, batch - {batch_id}, loss - {loss}')
 
         save_model(model, 'trained_models', f'{target}_iou_loss_ep_{epoch_id}')
+        print(get_test_score(val_loader, model, target))
         scheduler.step()
     return model
 
 def main():
-    train_loader, test_loader = get_dataset_loaders()
+    train_loader, val_loader, test_loader = get_dataset_loaders()
 
     # Training a single layer do detect instruments
     base_model = models.resnet18(weights='DEFAULT')
     base_model = base_model.requires_grad_(False)
 
-    note_detect_model = train_single_layer(
-        base_model, train_loader, TARGET_NOTES, lr=1e-2)
-    save_model(note_detect_model, 'trained_models', 
-        f'note_detect_single_layer_{N_EPOCHS}_epoch')
-    note_test_score = get_test_score(test_loader, note_detect_model, TARGET_NOTES)
-    print(f'test score for notes detection - {note_test_score}')
-
     inst_detect_model = train_single_layer(
-        base_model, train_loader, TARGET_INST)
+        base_model, train_loader, val_loader, TARGET_INST)
     save_model(inst_detect_model, 'trained_models', 
         f'instrument_detect_single_layer_{N_EPOCHS}_epoch')
     inst_test_score = get_test_score(test_loader, inst_detect_model, TARGET_INST)
     print(f'Perfect identification - {inst_test_score}')
     print(f'Instruments identified correctly - {inst_test_score}')
 
-
+    note_detect_model = train_single_layer(
+        base_model, train_loader, val_loader, TARGET_NOTES, lr=1e-2)
+    save_model(note_detect_model, 'trained_models', 
+        f'note_detect_single_layer_{N_EPOCHS}_epoch')
+    note_test_score = get_test_score(test_loader, note_detect_model, TARGET_NOTES)
+        print(f'test score for notes detection - {note_test_score}')
 
 if __name__ == '__main__':
     main()
